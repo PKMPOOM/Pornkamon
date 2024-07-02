4da87f13df7a6ffe99be956d67d0799f461ed2fa
PKMPOOM <loikpoom@gmail.com>
2024-07-02 20:44:46 +0700

    feat: Add Google Analytics tracking code to GA component

--- components/GA.tsx
+++ components/GA.tsx
@@ -5,11 +5,23 @@ type Props = {};
 const GA = (props: Props) => {
   return (
     <>
+      <Script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PTLXKWM');`}</Script>
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-5X0YS23L16"
       ></Script>
-
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-PTLXKWM"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
       <Script id="" strategy="lazyOnload">
         {`
             window.dataLayer = window.dataLayer || [];
