496b5c65add4b2ef78a48110f3856ffd63aa9490
PKMPOOM <loikpoom@gmail.com>
2024-07-02 20:14:47 +0700

    refactor: update page layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <div className=" flex  justify-center gap-6">
-      <div className=" flex w-full max-w-7xl flex-col justify-center gap-10 bg-green-50 px-6 pt-20 lg:flex-row  ">
+    <div className=" flex w-full justify-center gap-6 ">
+      <div className=" flex w-full max-w-7xl flex-col justify-center gap-10  px-6 pt-20 lg:flex-row  ">
         {/* left navbar */}
         <Details />
         {/* detail experiences */}
